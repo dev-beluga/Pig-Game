9ab361eeb2bd8800d6529e57fc04231138eb12ca
Israel Fitsum <fafisru@gmail.com>
2023-01-09 23:33:32 +0300

    dice-rolling

--- starter/script.js
+++ starter/script.js
@@ -4,8 +4,22 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 //starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+// Rolling Dice functionality
+
+btnRoll.addEventListener('click', function () {
+  //1.Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+  //   2.Display Dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+});
